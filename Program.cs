580a99501e9a06382177691e5bd05dc0ea1fa19a
neznej143 <jenzen.aglit@pointwest.com.ph>
2022-04-20 11:12:10 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySecondRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MY FIRST REPO");
+        }
+    }
+}
